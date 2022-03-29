375889a62a00893a480ad07ff4c8a29cdb914a3f
Ethan Bayer <byeres1871@gmail.com>
2022-03-29 16:40:45 -0700

    Task 4

--- calc.c
+++ calc.c
@@ -352,8 +352,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 11
-#define YY_END_OF_BUFFER 12
+#define YY_NUM_RULES 14
+#define YY_END_OF_BUFFER 15
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -361,10 +361,11 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[18] =
+static const flex_int16_t yy_accept[29] =
     {   0,
-        0,    0,   12,   10,   11,    5,    6,    4,    1,    2,
-        3,    8,    7,    0,    8,    9,    0
+        0,    0,   15,   13,   14,    5,    6,    4,    1,    2,
+       13,    3,    8,    7,   10,   12,    8,    0,    0,    9,
+        0,    0,   11,    0,   11,    0,   12,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -375,14 +376,14 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    3,
         4,    5,    6,    1,    7,    8,    9,   10,   10,   10,
        10,   10,   10,   10,   10,   10,   10,    1,    1,    1,
-       11,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+       11,    1,    1,    1,    1,    1,    1,    1,   12,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
 
+       12,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,   13,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -399,36 +400,44 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[12] =
+static const YY_CHAR yy_meta[14] =
+    {   0,
+        1,    1,    1,    1,    1,    2,    2,    1,    1,    2,
+        1,    1,    3
+    } ;
+
+static const flex_int16_t yy_base[31] =
+    {   0,
+        0,    0,   37,   38,   38,   38,   38,   38,   38,   38,
+       26,   38,    6,   38,   25,   10,    0,   17,   24,    0,
+       23,   19,   18,   16,   15,   11,    7,   38,   29,   13
+    } ;
+
+static const flex_int16_t yy_def[31] =
+    {   0,
+       28,    1,   28,   28,   28,   28,   28,   28,   28,   28,
+       28,   28,   28,   28,   28,   28,   13,   28,   29,   16,
+       30,   28,   28,   28,   28,   28,   28,    0,   28,   28
+    } ;
+
+static const flex_int16_t yy_nxt[52] =
+    {   0,
+        4,    5,    6,    7,    8,    9,   10,   11,   12,   13,
+       14,    4,    4,   16,   26,   17,   27,   18,   19,   20,
+       27,   21,   22,   22,   25,   25,   23,   23,   23,   24,
+       24,   24,   27,   25,   15,   15,   28,    3,   28,   28,
+       28,   28,   28,   28,   28,   28,   28,   28,   28,   28,
+       28
+    } ;
+
+static const flex_int16_t yy_chk[52] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1
-    } ;
-
-static const flex_int16_t yy_base[18] =
-    {   0,
-        0,    0,   16,   17,   17,   17,   17,   17,   17,   17,
-       17,    4,   17,    5,    0,    3,   17
-    } ;
-
-static const flex_int16_t yy_def[18] =
-    {   0,
-       17,    1,   17,   17,   17,   17,   17,   17,   17,   17,
-       17,   17,   17,   17,   12,   17,    0
-    } ;
-
-static const flex_int16_t yy_nxt[29] =
-    {   0,
-        4,    5,    6,    7,    8,    9,   10,    4,   11,   12,
-       13,   14,   16,   15,   16,   17,    3,   17,   17,   17,
-       17,   17,   17,   17,   17,   17,   17,   17
-    } ;
-
-static const flex_int16_t yy_chk[29] =
-    {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,   12,   16,   12,   14,    3,   17,   17,   17,   17,
-       17,   17,   17,   17,   17,   17,   17,   17
+        1,    1,    1,   13,   30,   13,   27,   13,   13,   16,
+       26,   16,   18,   18,   25,   24,   18,   23,   22,   18,
+       29,   29,   21,   19,   15,   11,    3,   28,   28,   28,
+       28,   28,   28,   28,   28,   28,   28,   28,   28,   28,
+       28
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -450,10 +459,10 @@ char *yytext;
    /* cs152-calculator */
    /* write your C code here for definition of variables and including headers */
    int arr[4] = {0, 0, 0, 0};
-#line 454 "calc.c"
+#line 463 "calc.c"
 #line 10 "calc.lex"
    /* some common rules, for example DIGIT (RULES section)*/
-#line 457 "calc.c"
+#line 466 "calc.c"
 
 #define INITIAL 0
 
@@ -673,7 +682,7 @@ YY_DECL
 #line 13 "calc.lex"
 
    /* specific lexer rules in regex */
-#line 677 "calc.c"
+#line 686 "calc.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -700,13 +709,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 18 )
+				if ( yy_current_state >= 29 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 17 );
+		while ( yy_base[yy_current_state] != 38 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -733,22 +742,22 @@ do_action:	/* This label is used only to access EOF actions. */
 case 1:
 YY_RULE_SETUP
 #line 15 "calc.lex"
-{printf("PLUS"); arr[1]++;}
+{printf("PLUS "); arr[1]++;}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
 #line 16 "calc.lex"
-{printf("MINUS"); arr[1]++;}
+{printf("MINUS "); arr[1]++;}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
 #line 17 "calc.lex"
-{printf("DIV"); arr[1]++;}
+{printf("DIV "); arr[1]++;}
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
 #line 18 "calc.lex"
-{printf("MULT"); arr[1]++;}
+{printf("MULT "); arr[1]++;}
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
@@ -778,14 +787,29 @@ YY_RULE_SETUP
 case 10:
 YY_RULE_SETUP
 #line 24 "calc.lex"
-{printf("ERROR, NOT RECOGNIZED SYMBOL\n"); exit(0);}
+{printf("DECIMAL %s\n", yytext); arr[0]++; }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
 #line 25 "calc.lex"
+{printf("SCIENTIFIC %s\n", yytext); arr[0]++; }
+	YY_BREAK
+case 12:
+YY_RULE_SETUP
+#line 26 "calc.lex"
+{printf("COMBINED NUMBER %s\n", yytext); arr[0]++;}
+	YY_BREAK
+case 13:
+YY_RULE_SETUP
+#line 27 "calc.lex"
+{printf("ERROR, NOT RECOGNIZED SYMBOL\n"); exit(0);}
+	YY_BREAK
+case 14:
+YY_RULE_SETUP
+#line 28 "calc.lex"
 ECHO;
 	YY_BREAK
-#line 789 "calc.c"
+#line 813 "calc.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1082,7 +1106,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 18 )
+			if ( yy_current_state >= 29 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1110,11 +1134,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 18 )
+		if ( yy_current_state >= 29 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 17);
+	yy_is_jam = (yy_current_state == 28);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1790,7 +1814,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 25 "calc.lex"
+#line 28 "calc.lex"
 
 	/* C functions used in lexer, runs the rules section */
 
--- calc.lex
+++ calc.lex
@@ -11,17 +11,19 @@ DIGIT    [0-9]
    
 %%
    /* specific lexer rules in regex */
-"+"                  {printf("PLUS "); arr[1]++;}
-"-"                  {printf("MINUS "); arr[1]++;}
-"/"                  {printf("DIV "); arr[1]++;}
-"*"                  {printf("MULT "); arr[1]++;}
-"("                  {printf("L_PAREN\n"); arr[2]++;}
-")"                  {printf("R_PAREN "); arr[2]++;}
-"="                  {printf("EQUAL\n"); arr[3]++;}
-{DIGIT}+             {printf("NUMBER %s\n", yytext); arr[0]++; }
-{DIGIT}+\.{DIGIT}+   {printf("DECIMAL %s\n", yytext); arr[0]++; }
-
-.                    {printf("ERROR, NOT RECOGNIZED SYMBOL\n"); exit(0);}
+"+"                              {printf("PLUS "); arr[1]++;}
+"-"                              {printf("MINUS "); arr[1]++;}
+"/"                              {printf("DIV "); arr[1]++;}
+"*"                              {printf("MULT "); arr[1]++;}
+"("                              {printf("L_PAREN\n"); arr[2]++;}
+")"                              {printf("R_PAREN "); arr[2]++;}
+"="                              {printf("EQUAL\n"); arr[3]++;}
+{DIGIT}+                         {printf("NUMBER %s\n", yytext); arr[0]++; }
+{DIGIT}+\.{DIGIT}+               {printf("DECIMAL %s\n", yytext); arr[0]++; }
+\.{DIGIT}+                       {printf("DECIMAL %s\n", yytext); arr[0]++; }
+{DIGIT}+[E|e][+|-]?{DIGIT}+      {printf("SCIENTIFIC %s\n", yytext); arr[0]++; }
+(\.{DIGIT}+)|({DIGIT}+(\.{DIGIT}*)?([eE][+-]?[0-9]+)?)   {printf("COMBINED NUMBER %s\n", yytext); arr[0]++;}
+.                                {printf("ERROR, NOT RECOGNIZED SYMBOL\n"); exit(0);}
 %%
 	/* C functions used in lexer, runs the rules section */
 
