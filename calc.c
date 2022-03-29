c5e6e75ba7edd09dd7c6a95254830ebb20fad2bf
Ethan Bayer <byeres1871@gmail.com>
2022-03-29 16:00:08 -0700

    Task 2

--- calc.c
+++ calc.c
@@ -1790,6 +1790,18 @@ void yyfree (void * ptr )
 
 int main(int argc, char ** argv)
 {
+   if (argc >= 2)
+   {
+      yyin = fopen(argv[1], "r");
+      if (yyin == NULL)
+      {
+         yyin = stdin;
+      }
+   }
+   else
+   {
+      yyin = stdin;
+   }
    yylex();
 }
 
